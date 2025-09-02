272420429b9367d11805f66aef876feab674a333
yoobright <yxj3179@gmail.com>
2025-09-02 17:48:10 +0800

    use types app

--- src/service/base.ts
+++ src/service/base.ts
@@ -1,23 +1,9 @@
 import { API_PREFIX } from '@/config'
 import Toast from '@/components/toast'
 import type { AnnotationReply, MessageEnd, MessageReplace, ThoughtItem } from '@/components/chat-type'
-// import type { VisionFile } from '@/types/app'
+import type { VisionFile } from '@/types/app'
 import type { OpenAIResponse } from '@/types/openai'
 
-export enum TransferMethod {
-  all = 'all',
-  local_file = 'local_file',
-  remote_url = 'remote_url',
-}
-
-export type VisionFile = {
-  id?: string
-  type: string
-  transfer_method: TransferMethod
-  url: string
-  upload_file_id: string
-  belongs_to?: string
-}
 
 const TIME_OUT = 100000
 
